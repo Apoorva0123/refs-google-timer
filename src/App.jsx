7dd5b5acff53551b0d0c6f2b550fb0f9f2f8382a
Apoorva0123 <47804749+Apoorva0123@users.noreply.github.com>
2022-04-24 09:44:11 +0530

    Google_timer

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [counter , setCounter] = useState(10);
+  const counterRef = useRef(null);
+  const startInterval = () => {
+    counterRef.current = setInterval(() => {
+      setCounter((p) => p + 1);
+    }, 1000)
+  }
+  useEffect(() => {
+      startInterval();
+  }, [])
+  
+  return (
+    <div className='App'>
+      <h3>Counter: {counter}</h3>
+      <button onClick={() => {
+        clearInterval(counterRef.current);
+      }}>Pause</button>
+
+      <button onClick={() => {
+        startInterval();
+      }}>Start</button>
+
+      <button onClick={() => {
+        clearInterval(counterRef.current);
+        setCounter(0);
+      }}>Reset</button>
+    </div>
+  )
+}
+
+export default App;
+
+//1. Store the value and do not lose it when component is re-rendering
+//2. change in this value should not cause re-render
